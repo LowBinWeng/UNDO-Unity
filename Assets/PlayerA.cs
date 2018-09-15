32d9bd66b88e6350e757727da7803458c84e3f28
J.Kae <jinkakukae@gmail.com>
2018-09-15 12:41:03 +0800

    J.Kae_Script

---
+++ Assets/PlayerA.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerA : MonoBehaviour {
+
+    float speed = 10f; 
+
+	// Use this for initialization
+	void Start () {
+		
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.D))Move(Vector2.right);
+	}
+
+     void Move(Vector2 dir)
+    {
+        transform.Translate(dir * speed * Time.deltaTime);
+    }
+}
